1028dcf46c678b82caa724cefb4dd3d1625fed8f
Nguyen Tran Tan Phat <touliver1609@gmail.com>
2017-10-04 11:01:28 +0700

    Add files via upload

---
+++ BTH5_Bai4/BTH5_Bai4/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BTH5_Bai4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.txtTenloai = new System.Windows.Forms.TextBox();
+            this.txtMaloai = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            this.gb1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.txtTenloai);
+            this.gb1.Controls.Add(this.txtMaloai);
+            this.gb1.Controls.Add(this.label2);
+            this.gb1.Controls.Add(this.label1);
+            this.gb1.Location = new System.Drawing.Point(19, 46);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(253, 134);
+            this.gb1.TabIndex = 0;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "Loại sản phẩm";
+            // 
+            // txtTenloai
+            // 
+            this.txtTenloai.Location = new System.Drawing.Point(73, 66);
+            this.txtTenloai.Name = "txtTenloai";
+            this.txtTenloai.Size = new System.Drawing.Size(100, 20);
+            this.txtTenloai.TabIndex = 3;
+            // 
+            // txtMaloai
+            // 
+            this.txtMaloai.Location = new System.Drawing.Point(73, 32);
+            this.txtMaloai.Name = "txtMaloai";
+            this.txtMaloai.Size = new System.Drawing.Size(100, 20);
+            this.txtMaloai.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên loại";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã loại ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btXoa);
+            this.panel1.Controls.Add(this.btSua);
+            this.panel1.Controls.Add(this.btThem);
+            this.panel1.Location = new System.Drawing.Point(28, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 64);
+            this.panel1.TabIndex = 1;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(162, 27);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(59, 21);
+            this.btXoa.TabIndex = 2;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.Location = new System.Drawing.Point(87, 27);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(59, 21);
+            this.btSua.TabIndex = 1;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Location = new System.Drawing.Point(13, 27);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(59, 21);
+            this.btThem.TabIndex = 0;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Location = new System.Drawing.Point(19, 287);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.Size = new System.Drawing.Size(478, 156);
+            this.dgvCategory.TabIndex = 2;
+            this.dgvCategory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategory_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 517);
+            this.Controls.Add(this.dgvCategory);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb1;
+        private System.Windows.Forms.TextBox txtTenloai;
+        private System.Windows.Forms.TextBox txtMaloai;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.DataGridView dgvCategory;
+    }
+}
+
---
+++ BTH5_Bai4/BTH5_Bai4/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BTH5_Bai4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sc = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string stn = "Server = .; Database = QLBanHang; Integrated Security = true";
+            sc = new SqlConnection(stn);
+            dgvCategory.DataSource = GetData();
+        }
+
+        private void Connect()
+        {
+           
+            try
+            {
+                if (sc != null && sc.State != ConnectionState.Open)
+                    sc.Open();
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Kết nối đã mở hoặc chưa có CSDL" + ex.Message);
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Sai mật khẩu" + ex.Message);
+            }
+
+            catch (ConfigurationErrorsException ex)
+            {
+                MessageBox.Show("Trùng tên" + ex.Message);
+            }  
+
+        }
+
+        private void DisConnect()
+        {
+            if (sc != null && sc.State != ConnectionState.Closed)
+            {
+                sc.Close();
+            }
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+
+            string str = "DELETE FROM LoaiSP WHERE MaLoaiSP = " + txtMaloai.Text;
+            SqlCommand cmd = new SqlCommand(str, sc);
+            Connect();
+            int sodong = cmd.ExecuteNonQuery();
+            dgvCategory.DataSource = GetData();
+            MessageBox.Show("So dong da xoa : " + sodong.ToString());
+            DisConnect();
+        }
+
+        private List<object> GetData()
+        {
+            Connect();
+            string sql = "SELECT * FROM LoaiSP";
+            List<object> list = new List<object>();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, sc);
+                SqlDataReader dr = cmd.ExecuteReader();
+                int id;
+                string name;
+                while (dr.Read())
+                {
+                    id = dr.GetInt32(0);
+                    name = dr.GetString(1);
+                    var proid = new
+                    {
+                        MaLoaiSP = id,
+                        TenLoaiSP = name
+                    };
+                    list.Add(proid);
+                }
+                dr.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Loi");
+            }
+            finally
+            {
+                DisConnect();
+            }
+            return list;
+        }
+
+        private void dgvCategory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
